d4e2b80e79452eef1320028daae693eaf5af8fca
Minsuchoi-1999 <102347404+Minsuchoi-1999@users.noreply.github.com>
2022-04-05 19:53:21 +0900

    restore old one

--- cal.sh
+++ cal.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
-
 num1=$(<num1.txt)
 num2=$(<num2.txt)
-
-declare -a numbers
-numbers=($num1 $num2)
 
 if [[ -z $operator || $operator = '' ]]; then operator="$1"; fi
 if [[ -z $operator || $operator = '' ]];
 	        then echo -e "...none operator parameter...\n1) add\n2) sub\n3) div\n4) mul\n select menu :";
 			read operator; fi
 
-echo "num1 : ${numbers[0]}";
-echo "num2 : ${numbers[1]}";					
+echo "num1 : $num1";
+echo "num2 : $num2";					
 
 case $operator in
-        1)let total=${numbers[0]}+${numbers[1]};
+        1)let total=$num1+$num2;
          echo -e "op : +\nresult : $total";;
-        2)let total=${numbers[0]}-${numbers[1]};
+        2)let total=$num1-$num2;
          echo -e "op : -\nresult : $total";;
-        3)let total=${numbers[0]}/${numbers[1]}; 
+        3)let total=$num1/$num2; 
          echo -e "op : /\nresult : $total";;
         4)let total=$num1*$num2; 
          echo -e "op : *\nresult : $total";;
